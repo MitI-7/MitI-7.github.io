60716627b6a08b19378260929f1a3bf1e1e9206b
MitI_7 <MitI_7@outlook.com>
2015-09-01 16:56:19 +0900

    解いた数を表示するようにした

--- TopCoderSRM.js
+++ TopCoderSRM.js
@@ -20,6 +20,16 @@ function readJson(url) {
     return JSON.parse(xmlhttp.responseText);
 }
 
+function count(solved) {
+    var k = {'DIV2_Easy': 0, 'DIV2_Normal': 0, 'DIV2_Hard': 0, 'DIV1_Easy': 0, 'DIV1_Normal': 0, 'DIV1_Hard': 0};
+    for (var d in solved) {
+        var t = d.split("_")[1] + '_' + d.split("_")[2];
+        //alert(t);
+        k[t] += 1;
+    }
+    return k;
+}
+
 $(function(){
 
     var problemData = readJson("./data/TopCoder/allProblem.json");
@@ -28,6 +38,16 @@ $(function(){
     problemData = sortDict(problemData);
 
     var html = "";
+
+    html += '<tr><td>solved</td>';
+    var cou = count(solved);
+    var n = Object.keys(problemData).length;
+    var l = ['DIV2_Easy', 'DIV2_Normal', 'DIV2_Hard', 'DIV1_Easy', 'DIV1_Normal', 'DIV1_Hard'];
+    for (var i = 0; i < l.length; ++i) {
+        html += '<td>' + cou[l[i]] + '/' + n + '</td>';
+    }
+    html += '</tr>';
+
     // srm 000
     for (var contest in problemData) {
         var contestUrl = 'http://community.topcoder.com/' + problemData[contest]['url'];
@@ -44,15 +64,18 @@ $(function(){
                 var problem = array[i];
 
                 var td = '<td>';
+                var date = "";
                 var tag = contest + "_" + division + "_" + problem['level'];
-                if (solved.indexOf(tag) != -1) {
+                if (tag in solved) {
                     td = '<td bgcolor="#98FB98">';
+                    date = "Solved:" + solved[tag];
                 }
+
                 else if (review.indexOf(tag) != -1) {
                     td = '<td bgcolor="#FF6347">';
                 }
                 var problemUrl = 'http://community.topcoder.com/' + problem['url'];
-                level[problem['level']] = td + '<a href="' + problemUrl + '" target="_blank">' +  problem['problemName'] + '</a>' + '<br>' + problem['overall'] + '</td>';
+                level[problem['level']] = td + '<a href="' + problemUrl + '" target="_blank">' +  problem['problemName'] + '</a>' + '<br>' + problem['overall'] + '<br>' + date +'</td>';
             }
             divisionDict[division]= level['Easy'] + level['Normal'] + level['Hard']
         }
